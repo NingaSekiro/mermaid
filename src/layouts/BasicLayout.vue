a7aecec8632ee4d5f445c49b310814c96f38d351
q30049576 <arjumantaiba154@gmail.com>
2025-11-02 03:46:58 +0800

    tmp

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,21 +1,15 @@
 <template>
-  <div class="basic-layout">
-    <a-layout>
-      <a-layout-header :style="{backgroundColor: 'transparent',paddingInline:'unset'}">
-        <Menu />
-      </a-layout-header>
-      <a-layout-content>
-        <router-view />
-      </a-layout-content>
-    </a-layout>
-  </div>
+  <a-layout style="height: 100%">
+    <a-layout-header :style="{ backgroundColor: 'transparent', paddingInline: 'unset' }">
+      <Menu />
+    </a-layout-header>
+    <a-layout-content>
+      <router-view />
+    </a-layout-content>
+  </a-layout>
 </template>
 <script setup>
-
 import Menu from '@/views/Menu.vue'
 </script>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,72 @@
 <template>
-  <a-collapse v-model:activeKey="activeOutKey" @change="updateMethodChains(activeOutKey)" accordion>
-    <a-collapse-panel
-      v-for="(record, index) in methodStore.methodRecords"
-      :key="index"
-      :header="record"
+  <a-layout class="main-layout" style="height: 100%">
+    <!-- 左侧侧边栏 -->
+    <a-layout-sider
+      width="320"
+      :style="{
+        overflow: 'auto',
+        backgroundColor: 'transparent',
+        borderRight: '1px solid rgba(255, 255, 255, 0.12)',
+      }"
     >
-      <a-collapse
-        v-model:activeKey="activeInKey"
-        @change="updateMermaidCode(activeInKey)"
-        accordion
+      <div style="padding: 16px">
+        <a-collapse v-model:activeKey="activeOutKey" @change="updateMethodChains(activeOutKey)">
+          <a-collapse-panel v-for="(record, index) in methodStore.methodRecords" :key="index">
+            <template #header>
+              <a-typography-text
+                :ellipsis="{ tooltip: true }"
+                :content="record"
+              >
+              </a-typography-text>
+            </template>
+            <a-list :data-source="methodStore.methodChains" :bordered="false" size="small">
+              <template #renderItem="{ item, index: itemIndex }">
+                <a-list-item
+                  @click="updateMermaidCode(itemIndex)"
+                  :style="{
+                    cursor: 'pointer',
+                    padding: '4px 0',
+                  }"
+                >
+                  <a-typography-text
+                    :ellipsis="{ tooltip: true }"
+                    style="display: block; width: 100%"
+                    :content="item.methodChain"
+                  >
+                  </a-typography-text>
+                </a-list-item>
+              </template>
+            </a-list>
+          </a-collapse-panel>
+        </a-collapse>
+      </div>
+    </a-layout-sider>
+
+    <!-- 主内容区域 -->
+    <a-layout-content class="main-content">
+      <div
+        :style="{
+          padding: '24px',
+        }"
       >
-        <a-collapse-panel
-          v-for="(item, itemIndex) in methodStore.methodChains"
-          :key="itemIndex"
-          :header="item.methodChain"
+        <MermaidRenderer v-if="methodStore.mermaidCode" :mermaid-code="methodStore.mermaidCode" />
+        <div
+          v-else
+          style="height: 100%; display: flex; align-items: center; justify-content: center"
         >
-          <MermaidRenderer :mermaid-code="methodStore.mermaidCode" />
-        </a-collapse-panel>
-      </a-collapse>
-    </a-collapse-panel>
-  </a-collapse>
+          <a-empty description="请从左侧选择一个方法链查看图表" />
+        </div>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
+
 <script setup>
 import { onMounted, ref } from 'vue'
 import { useMethodStore } from '@/stores/useMethodStore.js'
 import MermaidRenderer from '@/components/MermaidRenderer.vue'
 
 const activeOutKey = ref([])
-const activeInKey = ref([])
 const methodStore = useMethodStore()
 
 onMounted(() => {
@@ -39,18 +78,20 @@ const updateMethodRecords = async () => {
 }
 
 const updateMethodChains = async (item) => {
-  if (item === undefined) {
+  if (item === undefined || item.length === 0) {
     return
   }
-  await methodStore.getMethodChains(methodStore.methodRecords[item])
+  await methodStore.getMethodChains(methodStore.methodRecords[item[0]])
 }
 
-const updateMermaidCode = async (item) => {
-  if (item === undefined) {
+const updateMermaidCode = async (index) => {
+  if (index === undefined) {
     return
   }
-  await methodStore.getMermaidCode(methodStore.methodChains[item].methodChain)
+  await methodStore.getMermaidCode(methodStore.methodChains[index].methodChain)
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
