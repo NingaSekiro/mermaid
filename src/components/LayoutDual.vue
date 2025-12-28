62ed1cae3d35a999dbee007eebb627fa12809c00
q30049576 <arjumantaiba154@gmail.com>
2025-12-28 22:41:09 +0800

    Revert "引入element 分隔面板"
    This reverts commit 502b320a

--- src/components/LayoutDual.vue
+++ src/components/LayoutDual.vue
@@ -1,19 +1,28 @@
 <template>
-  <el-splitter>
-    <el-splitter-panel size="20%">
+  <splitpanes class="default-theme">
+    <pane size="20">
       <div style="padding: 8px; display: grid; gap: 6px">
         <slot name="panel" />
       </div>
-    </el-splitter-panel>
+    </pane>
 
-    <el-splitter-panel size="80%">
+    <pane size="80">
       <div :style="{ padding: '24px' }">
         <slot name="chart" />
       </div>
-    </el-splitter-panel>
-  </el-splitter>
+    </pane>
+  </splitpanes>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style>
+.default-theme.splitpanes .splitpanes__splitter {
+  background-color: transparent !important;
+}
+
+.default-theme.splitpanes .splitpanes__pane {
+  background-color: transparent !important;
+}
+
+</style>
